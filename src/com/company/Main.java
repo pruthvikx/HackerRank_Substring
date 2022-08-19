ca9e3c2fcf005d9962e4aafe4b32c5e9fb9f2af8
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-08-19 16:58:57 +0530

    hackerrank substring challenge

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+    }
+}
